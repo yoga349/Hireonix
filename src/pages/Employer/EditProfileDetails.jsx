75ee4ddbe5ff9f44861a3bc1f0d0b93e84366038
yoga349 <yp14115@gmail.com>
2026-03-20 16:42:16 +0530

    App routes created

---
+++ src/pages/Employer/EditProfileDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditProfileDetails = () => {
+  return (
+    <div>EditProfileDetails</div>
+  )
+}
+
+export default EditProfileDetails
---
+++ src/pages/Employer/JobPostingForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobPostingForm = () => {
+  return (
+    <div>JobPostingForm</div>
+  )
+}
+
+export default JobPostingForm
